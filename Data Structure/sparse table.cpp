d30553e07c98e5b2efac40ccbe5fb9345a0637ec
Eric <ericncnl3742@gmail.com>
2024-08-21 13:13:45 +0800

    Modify definition of sparse table

--- Data Structure/sparse table.cpp
+++ Data Structure/sparse table.cpp
@@ -1,4 +1,4 @@
-template<typename T, T (*f)(T&, T&), T e>
+template<typename T, T (*f)(T, T), T (*e)()>
 struct sparse_table {
     int lv, n;
     vector<vector<T> > t;
@@ -10,14 +10,16 @@ struct sparse_table {
                 t[i][j] = f(t[i - 1][j], t[i - 1][j + (1 << i - 1)]);
     }
 
-    T query(int l, int r) {
-        T ret = e;
+    T query_general(int l, int r) {
+        T ret = e();
         for (int i = lv - 1; i >= 0; i--)
             if (r - l + 1 >= 1 << i)
                 ret = f(ret, t[i][l]), l += 1 << i;
         return ret;
-        // O(1) query : only works for specified operation
-        // int i = __lg(r - l + 1);
-        // return f(t[i][l], t[i][r - (1 << i) + 1]);
+    }
+
+    T query(int l, int r) {
+        int i = __lg(r - l + 1);
+        return f(t[i][l], t[i][r - (1 << i) + 1]);
     }
 };
