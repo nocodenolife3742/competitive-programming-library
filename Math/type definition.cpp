758dce0bae40566f4bc8319087446f9370827559
Eric <ericncnl3742@gmail.com>
2024-09-03 01:45:11 +0800

    Change definition of int128

--- Math/type definition.cpp
+++ Math/type definition.cpp
@@ -1,6 +1,6 @@
 using i32 = int;
 using i64 = long long;
+using i128 = __int128;
 using u32 = unsigned int;
 using u64 = unsigned long long;
-using i128 = __int128_t;
-using u128 = __uint128_t;
+using u128 = unsigned __int128;
