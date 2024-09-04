8797e8eea39bb053a5ecb728208885ca2806d9ec
Eric <ericncnl3742@gmail.com>
2024-09-05 01:58:03 +0800

    Fix bug of convolution

--- Math/convolution(without mod).cpp
+++ Math/convolution(without mod).cpp
@@ -4,8 +4,8 @@ vector<i64> convolution(const vector<i64> &a, const vector<i64> &b) {
     vector<i64> ret(a.size() + b.size() - 1);
     int n = 2 << __lg(ret.size());
     vector<complex<double> > in(n), out(n);
-    for (int i = 0; i < a.size(); i++)
-        in[i] = {1.0 * a[i], 1.0 * b[i]};
+    for (int i = 0; i < a.size(); i++) in[i].real(a[i]);
+    for (int i = 0; i < b.size(); i++) in[i].imag(b[i]);
     fft(in);
     for (int i = 0; i < n; i++) in[i] *= in[i];
     for (int i = 0; i < n; i++)
