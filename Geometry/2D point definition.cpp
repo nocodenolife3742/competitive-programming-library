3b8f3a50b6138d97738faabaf336eec1f75632a4
Eric <ericncnl3742@gmail.com>
2024-09-06 20:05:30 +0800

    Add point operations

--- Geometry/2D point definition.cpp
+++ Geometry/2D point definition.cpp
@@ -1,13 +1,47 @@
 template<typename T>
 struct point {
     T x, y;
+
     point(T x = 0, T y = 0) : x(x), y(y) {}
-    template<typename O> operator point<O>() { return {x, y}; }
-    point operator+(const point &o) { return {x + o.x, y + o.y}; }
-    point operator-(const point &o) { return {x - o.x, y - o.y}; }
-    point operator*(const T &o) { return {x * o, y * o}; }
-    point operator/(const T &o) { return {x / o, y / o}; }
-    T operator*(const point &o) { return x * o.x + y * o.y; }
-    T operator/(const point &o) { return x * o.y - y * o.x; }
-    T norm() { return x * x + y * y; }
+
+    template<typename O>
+    operator point<O>() {
+        return {x, y};
+    }
+
+    operator bool() {
+        return abs(x) + abs(y) < 1e-9;
+    }
+
+    point operator+(const point &o) {
+        return {x + o.x, y + o.y};
+    }
+
+    point operator-(const point &o) {
+        return {x - o.x, y - o.y};
+    }
+
+    point operator*(const T &o) {
+        return {x * o, y * o};
+    }
+
+    point operator/(const T &o) {
+        return {x / o, y / o};
+    }
+
+    T operator*(const point &o) {
+        return x * o.x + y * o.y;
+    }
+
+    T operator/(const point &o) {
+        return x * o.y - y * o.x;
+    }
+
+    T norm() {
+        return x * x + y * y;
+    }
+
+    long double arg() {
+        return atan2l(y, x);
+    }
 };
