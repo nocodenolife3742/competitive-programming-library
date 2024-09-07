0e92c8d14687d8eea345d4e856251540387fc576
Eric <ericncnl3742@gmail.com>
2024-09-08 02:34:04 +0800

    Add Knuth–Morris–Pratt algorithm

---
+++ String/knuth morris pratt.cpp
@@ -0,0 +1,8 @@
+vector<int> kmp_nxt(string &s) {
+    vector<int> nxt(s.size(), 0);
+    for (int i = 1, j = 0; i < s.size(); nxt[i++] = j) {
+        while (j > 0 && s[i] != s[j]) j = nxt[j - 1];
+        if (s[i] == s[j]) j++;
+    }
+    return nxt;
+}
